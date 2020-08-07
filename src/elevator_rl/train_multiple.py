5a53c3000db255140871cb3dd3d98a2b07c25136
Tim Pfeifle <pfeifletim@gmail.com>
2020-08-07 22:25:53 +0200

    Configs for multiple elevators and more floors

--- src/elevator_rl/train_multiple.py
+++ src/elevator_rl/train_multiple.py
@@ -2,7 +2,14 @@ from elevator_rl.train import main
 
 
 def multiple():
-    configs = ["default", "8stores", "8stores_2elevators"]
+    # configs = ["default", "8stores", "8stores_2elevators"]
+    configs = [
+        "2elev_3floor",
+        "1elev_5floor",
+        "2elev_5floor",
+        "1elev_7floor",
+        "2elev_7floor",
+    ]
     for config in configs:
         main(config_name=config)
 
